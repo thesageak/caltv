bb02708dd59165218938963f1ec028706fae9fae
thesageak <sageaguinakang@gmail.com>
2025-11-26 06:51:30 -0800

    Enhance Join page with additional animations

--- app/section/join/page.tsx
+++ app/section/join/page.tsx
@@ -1,34 +1,101 @@
 'use client';
 
+import { easeInOut, motion, useScroll, useSpring, useTransform } from 'framer-motion'
+import { useEffect } from 'react';
 import mountain from '../../../public/peak_mountain.png'
-import { motion, useScroll, useSpring, useTransform } from 'framer-motion'
+import calTVCircle from '@/public/CalTVLogoBlackCircle.png'
 
 export default function Join() {
     const { scrollY } = useScroll();
-    const rawY1 = useTransform(scrollY, [0, 100], [0, 230]);
-    const y1 = useSpring(rawY1, { stiffness: 30, damping: 25 });
-    const rawY2 = useTransform(scrollY, [0, 500], [0, 70]);
-    const y2 = useSpring(rawY2, { stiffness: 50, damping: 25 });
+    const rawBackY = useTransform(scrollY, [0, 500], [0, 200]);
+    const backY = useSpring(rawBackY, { stiffness: 35, damping: 20 });
+    const rawMiddleY = useTransform(scrollY, [0, 500], [0, 100]);
+    const middleY = useSpring(rawMiddleY, { stiffness: 50, damping: 25 });
+
     return (
         <div>
-            <div className="relative w-full h-245 bg-gray-500 overflow-hidden">
+            <div className="relative w-full h-245 bg-lightGrey overflow-hidden">
+                <motion.div
+                    className="absolute w-px bottom-0 left-[55%] translate-x-50 -translate-y-20 bg-white"
+                    initial={{ height: 0 }}
+                    animate={{ height: "22.5rem" }}
+                    transition={{ delay: 0.6 }}
+                />
+                <motion.div
+                    className="absolute w-px bottom-0 left-[55%] translate-x-65 -translate-y-30 bg-white"
+                    initial={{ height: 0 }}
+                    animate={{ height: "25rem" }}
+                    transition={{ delay: 0.7 }}
+                />
+                <motion.div
+                    className="absolute w-px bottom-0 left-[55%] translate-x-80 -translate-y-40 bg-white"
+                    initial={{ height: 0 }}
+                    animate={{ height: "37.5rem" }}
+                    transition={{ delay: 0.8 }}
+                />
+
                 <motion.img
                     src={mountain.src}
-                    className="absolute w-full h-auto object-cover bottom-0 blur-sm translate-y-45 -scale-x-100"
-                    style={{ y: y1 }}
+                    className="absolute w-full h-auto object-cover bottom-0 blur-sm translate-y-40 -scale-x-100"
+                    initial={{ y: 200 }}
+                    animate={{ y: 0 }}
+                    transition={{ duration: 1, ease: [0.16, 1, 0.3, 1], delay: 0.1}}
+                    style={{ y: backY }}
                 />
                 <motion.img
                     src={mountain.src}
-                    className="absolute w-full h-auto object-cover bottom-0 translate-y-50"
-                    style={{ y: y2 }}
+                    className="absolute w-full h-auto object-cover bottom-0 translate-y-40"
+                    initial={{ y: 200 }}
+                    animate={{ y: 0 }}
+                    transition={{ duration: 1, ease: [0.16, 1, 0.3, 1], delay: 0.1}}
+                    style={{ y: middleY }}
                 />
                 <img
                     src={mountain.src}
                     className="absolute w-full h-auto object-cover bottom-0 blur-sm translate-y-90 -scale-x-120"
                 />
+
+                <motion.img
+                    src={calTVCircle.src}
+                    className="w-60 h-auto object-cover mt-10 ml-15"
+                    initial={{ clipPath: "circle(0% at 50% 50%)" }}   // start fully hidden
+                    animate={{ clipPath: "circle(100% at 50% 50%)" }}   // reveal to half radius (adjust as needed)
+                    transition={{ ease: "easeIn", duration: 0.25, delay: 0.5 }}
+                />
+
+                <motion.div
+                    className="flex flex-col mx-75"
+                    initial={{ y: 1000 }}
+                    animate={{ y: 0 }}
+                    transition={{
+                        duration: 1,
+                        ease: [0.16, 1, 0.3, 1]
+                    }}
+                >
+                    <h1 className="text-white text-[4.5rem] font-extrabold leading-15 mb-7">
+                        REACH NEW <br /> HEIGHTS <br /> WITH <br /> US.
+                    </h1>
+                    <p className="text-white text-[1.2rem]">
+                        LEARN MORE
+                    </p>
+                </motion.div>
+
+                <motion.div
+                    className="absolute w-px bottom-0 left-[55%] translate-x-20 bg-white"
+                    initial={{ height: 0 }}
+                    animate={{ height: "17.5rem" }}
+                    transition={{ delay: 0.5 }}
+                />
+                <motion.div
+                    className="absolute w-px bottom-0 left-[55%] translate-x-35 -translate-y-10 bg-white"
+                    initial={{ height: 0 }}
+                    animate={{ height: "17.5rem" }}
+                    transition={{ delay: 0.4 }}
+                />
+
             </div>
 
-            <div className="h-1000" bg-white />
+            <div className="h-300" bg-white />
 
         </div>
     )
