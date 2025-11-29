673764a116af87e2d6c6cd44a8dee15c7d7e9215
thesageak <sageaguinakang@gmail.com>
2025-11-28 16:56:36 -0800

    Add parllax effect to info session in Join page

--- app/section/join/page.tsx
+++ app/section/join/page.tsx
@@ -2,10 +2,13 @@
 
 import { motion, useScroll, useSpring, useTransform, AnimatePresence } from 'framer-motion'
 import { useState, useEffect } from 'react';
+import Footer from '@/components/Footer';
 import TextReveal from '@/components/TextReveal';
 import mountain from '../../../public/peak_mountain.png'
 import calTVCircle from '@/public/CalTVLogoBlackCircle.png'
 import officeImage from '@/public/office_cover.jpg'
+import colorBarAsset from '@/public/ColorBarAsset.svg'
+import colorBackAsset from '@/public/ColorBackAsset.svg'
 
 export default function Join() {
     const { scrollY } = useScroll();
@@ -19,13 +22,18 @@ export default function Join() {
     const [showLogo, setShowLogo] = useState(false);
 
     useEffect(() => {
-        if (index === texts.length - 1) return;
+        if (index === texts.length - 1)
+            return;
         const interval = setInterval(() => setIndex((prevIndex) => (prevIndex + 1) % texts.length), 4000);
-            return () => clearInterval(interval);
-        }, [index]);
+        return () => clearInterval(interval);
+    }, [index]);
+
+    const rawInfoY = useTransform(scrollY, [0,1000], [-100,100]);
+    const infoY = useSpring(rawInfoY, {stiffness:20, damping: 50});
 
     return (
         <div>
+            {/*-------------- Top Mountain Section --------------*/}
             <div className="relative w-full h-245 bg-lightGrey overflow-hidden">
                 <motion.div
                     className="absolute w-px bottom-0 left-[55%] translate-x-50 -translate-y-20 bg-white"
@@ -51,17 +59,19 @@ export default function Join() {
                     className="absolute w-full h-auto object-cover bottom-0 blur-sm translate-y-40 -scale-x-100"
                     initial={{ y: 200 }}
                     animate={{ y: 0 }}
-                    transition={{ duration: 1, ease: [0.16, 1, 0.3, 1], delay: 0.1}}
+                    transition={{ duration: 1, ease: [0.16, 1, 0.3, 1], delay: 0.1 }}
                     style={{ y: backY }}
                 />
+
                 <motion.img
                     src={mountain.src}
                     className="absolute w-full h-auto object-cover bottom-0 translate-y-40"
                     initial={{ y: 200 }}
                     animate={{ y: 0 }}
-                    transition={{ duration: 1, ease: [0.16, 1, 0.3, 1], delay: 0.1}}
+                    transition={{ duration: 1, ease: [0.16, 1, 0.3, 1], delay: 0.1 }}
                     style={{ y: middleY }}
                 />
+
                 <img
                     src={mountain.src}
                     className="absolute w-full h-auto object-cover bottom-0 blur-sm translate-y-90 -scale-x-120"
@@ -70,8 +80,8 @@ export default function Join() {
                 <motion.img
                     src={calTVCircle.src}
                     className="w-60 h-auto object-cover mt-10 ml-15"
-                    initial={{ clipPath: "circle(0% at 50% 50%)" }}  
-                    animate={{ clipPath: "circle(100% at 50% 50%)" }}  
+                    initial={{ clipPath: "circle(0% at 50% 50%)" }}
+                    animate={{ clipPath: "circle(100% at 50% 50%)" }}
                     transition={{ ease: "easeIn", duration: 0.25, delay: 0.5 }}
                 />
 
@@ -87,9 +97,9 @@ export default function Join() {
                     <h1 className="text-white text-[4.5rem] font-extrabold leading-15 mb-7">
                         REACH NEW <br /> HEIGHTS <br /> WITH <br /> US.
                     </h1>
-                    <p className="text-white text-[1.2rem]">
+                    <a href="#info-session" className="scroll-smooth text-white text-[1.2rem]">
                         LEARN MORE
-                    </p>
+                    </a>
                 </motion.div>
 
                 <motion.div
@@ -107,50 +117,67 @@ export default function Join() {
 
             </div>
 
-            {/* <div className="h-100" bg-white /> */}
+            <div className="h-20 bg-gray-400" />
 
-
-            <div className="relative flex justify-center items-center">
-                <img 
-                    src={officeImage.src} 
-                    alt="Office Cover" 
-                    className="w-full h-80 object-cover" 
+            {/* Info Session */}
+           <div id="info-session" className="relative w-full h-[500px] overflow-hidden">
+                <motion.img 
+                    src={colorBackAsset.src}
+                    className="absolute w-full h-[895px] object-cover object-[40%_50%] -translate-y-[15%]"
+                    style={{y: infoY}}
                 />
-                <div className="absolute flex justify-center items-center gap-4 translate-x-10">
-                    <p 
-                        className="text-white text-[4rem] font-extrabold"
-                    >
-                        WE LOVE
-                    </p>
-                    <div className="w-[30vw]">
-                        <AnimatePresence 
-                            mode="wait"
-                            onExitComplete ={() => {if (index === texts.length - 1) setShowLogo(true);}}
+            </div>
+
+            <div className="h-20 bg-gray-400" bg-white />
+
+            {/*-------------- Banner Section --------------*/}
+            <div className="flex flex-col">
+                <div className="relative flex justify-center items-center">
+                    <img
+                        src={officeImage.src}
+                        alt="Office Cover"
+                        className="w-full h-80 object-cover"
+                    />
+                    <div className="absolute flex justify-center items-center gap-4 translate-x-10">
+                        <p
+                            className="text-white text-[4rem] font-extrabold"
                         >
-                            <TextReveal 
-                                key={texts[index]}
-                                text={texts[index]}
-                                className="text-orange-500 text-[4rem] font-extrabold"
-                            />
-                        </AnimatePresence>
-                        { 
-                            showLogo && (
-                            <motion.img
-                                src={calTVCircle.src}
-                                initial={{ opacity: 0}}
-                                animate={{ opacity: 1 }}
-                                transition={{ duration: 0.5 }}
-                                className="w-60 h-auto"
-                            />
-                        )}
+                            WE LOVE
+                        </p>
+                        <div className="w-[30vw]">
+                            <AnimatePresence
+                                mode="wait"
+                                onExitComplete={() => { if (index === texts.length - 1) setShowLogo(true); }}
+                            >
+                                <TextReveal
+                                    key={texts[index]}
+                                    text={texts[index]}
+                                    className="text-orange-500 text-[4rem] font-extrabold"
+                                />
+                            </AnimatePresence>
+                            {
+                                showLogo && (
+                                    <motion.img
+                                        src={calTVCircle.src}
+                                        initial={{ opacity: 0 }}
+                                        animate={{ opacity: 1 }}
+                                        transition={{ duration: 0.5 }}
+                                        className="w-60 h-auto"
+                                    />
+                                )}
+                        </div>
                     </div>
                 </div>
+                <img
+                    src={colorBarAsset.src}
+                    alt="Color Bar Asset"
+                    className="w-full h-6 object-cover"
+                />
             </div>
-            
-            
 
             <div className="h-100" bg-white />
 
+            <Footer />
         </div>
     )
 }
